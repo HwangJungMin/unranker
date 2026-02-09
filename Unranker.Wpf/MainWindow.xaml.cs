1afc193a2dbd16bf99f4054cdbe8f11bd8d736b6
unranker <35517752+HwangJungMin@users.noreply.github.com>
2026-02-09 16:49:26 +0900

    Bind PLC operation mode text to Mode property

--- Unranker.Wpf/MainWindow.xaml.cs
+++ Unranker.Wpf/MainWindow.xaml.cs
@@ -2,13 +2,11 @@ namespace Unranker.Wpf;
 
 public partial class MainWindow : System.Windows.Window
 {
+    public string Mode { get; } = "AUTO";
+
     public MainWindow()
     {
         InitializeComponent();
-    }
-
-    private void OnConfirmClick(object sender, System.Windows.RoutedEventArgs e)
-    {
-        System.Windows.MessageBox.Show("WPF 앱이 정상적으로 실행 중입니다.", "Unranker", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Information);
+        DataContext = this;
     }
 }
